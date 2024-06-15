ec4c99c89aff73fe476d2284827d40f51f422d56
thesantg <santiago_calderon_vargas@hotmail.com>
2024-06-15 11:13:33 -0600

    actualizacion

---
+++ mokepon.js
@@ -0,0 +1,5 @@
+function seleccionarMascotaJugador (){
+    alert("funciona");
+}
+let botonMascotaJugador = document.getElementById("boton-mascota");
+botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
