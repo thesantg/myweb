216cbc67f991eb3a62176ca8ce876c9116916d7b
thesantg <santiago_calderon_vargas@hotmail.com>
2024-06-27 06:03:27 -0600

    actualizacion 27/06/2024

--- mokepon.js
+++ mokepon.js
@@ -1,3 +1,5 @@
+let ataqueJugador;
+let ataqueEnemigo;
 function cargarJuego(){
     let botonMascotaJugador = document.getElementById("boton-mascota"); /*el document.getElementById("id") se utiliza para manipular un elemento de html por su id */
         botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador); /*ahora que vinculamos el html con el js y con la variable "botonMascotaJugador ahora hay que darle una funcion"*/
@@ -13,7 +15,12 @@ function cargarJuego(){
     -mousedown = el usuairo presiona el boton del raton en un elemento
 
      */
-
+    let botonFuego = document.getElementById("boton-fuego");
+        botonFuego.addEventListener('click', ataqueFuego);
+    let botonAgua = document.getElementById("boton-agua");
+        botonAgua.addEventListener('click', ataqueAgua);
+    let botonTierra = document.getElementById("boton-tierra");
+        botonTierra.addEventListener('click', ataqueTierra);
 }
 function seleccionarMascotaJugador (){
     let inputHipodoge = document.getElementById("hipodoge").checked; //aquie lo que hacemos el darle una variable js al lemento getElementByID().checked que este nos va a arrajar un true o false si se esta seleccionado el elemento
@@ -22,13 +29,79 @@ function seleccionarMascotaJugador (){
     let spamMascotaJugador = document.getElementById("mascota-jugador");
     
     if(inputHipodoge){
+
         spamMascotaJugador.innerHTML = "Hipodoge"; //o que hacemos es cambiar la informacion dentro del html por el innerHTML
         }else if(inputCapipepo){
+
             spamMascotaJugador.inerHTML = "Capipepo"
             }else if(inputRatigueya){
-                spamMascotaJugador.innerHTML = "RAtigueya"
+
+                spamMascotaJugador.innerHTML = "Ratigueya"
             }else{
+
                 alet("Selecciona una mascota");
             }
+
+            SeleccionarMascontaEnemigo()
+}
+function SeleccionarMascontaEnemigo (){
+
+    let mascotaAleatoria = aletorio(1,3);
+    let spamMascotaEnemigo = document.getElementById("mascota-enemigo");
+        if(mascotaAleatoria == 1){
+            spamMascotaEnemigo.innerHTML("Hipodoge");
+            }else if(mascotaAleatoria == 2){
+                spamMascotaEnemigo.innerHTML("Capipepo");
+                }else{
+                    spamMascotaEnemigo.innerHTML("Ratigueya");
+                }
+}
+function ataqueFuego(){
+    ataqueJugador = 'Fuego';
+    ataqueAleatorioEnemigo();
+}
+function ataqueAgua(){
+    ataqueJugador = 'Agua';
+    ataqueAleatorioEnemigo();
+}
+function ataqueTierra(){
+    ataqueJugador = 'Tierra';
+    ataqueAleatorioEnemigo();
+}
+function ataqueAleatorioEnemigo(){
+    let AtaqueAleatorio = aleatorio(1,3);
+
+    if(AtaqueAleatorio == 1){
+        ataqueEnemigo = 'Fuego';
+        }else if(ataqueAleatorio == 2){
+            ataqueEnemigo = 'Agua';
+            }else{
+            ataqueEnemigo = 'Tierra';
+    }
+    crearMensaje();
+}
+
+function combate(){
+    if(ataqueEnemigo == ataqueJugador){
+        crearMensaje("Empate");
+        }else if(ataqueJugador == 'Fuego' && ataqueEnemigo == 'Tierra'){
+            crearMensaje('Ganaste');
+            } else if(ataqueJugador == 'Agua' && ataqueEnemigo == 'Fuego'){
+                crearMensaje('Ganaste');
+                }else if(ataqueJugador == 'Tierra' && ataqueEnemigo == 'Fuego'){
+                    crearMensaje('Ganaste');
+                }else{
+                    crearMensaje('Perdiste');
+                }
+
+}
+function crearMensaje(resultado){
+let sectionMensajes = document.getElementById('mensajes');
+ let parrafo = document.createElement('p'); //creamos una etiqueta que con el document.crearteElement('tag');
+ parrafo.innerHTML = "tu mascota ataco con "+ ataqueJugador + " la mascota del enemigo ataco con "+ ataqueEnemigo+ " "+ resultado;
+sectionMensajes.appendChild(parrafo); //aqui estamos metiendo el parrafo aun etiqueta section, con el id
+}
+function aleatorio (min, max){ 1
+    return Math.floor(Math.random()*(max - min + 1) + min);
 }
 window.addEventListener("load", cargarJuego); /*lo que case el evento "load* es lo mismo que el defer en la etiqueta script, carga el js asta que el html y el css an cargado*/
