2dbc8f1b5d60b3ab1f67f7933adcdb1f6f52d8e9
thesantg <santiago_calderon_vargas@hotmail.com>
2025-04-11 21:02:18 -0600

    actializacion 11/04/2025

--- 2035-logica-programacion-2-Aula1 (1)/2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1 (1)/2035-logica-programacion-2-Aula1/app.js
@@ -1,5 +1,8 @@
 let numeroSecreto = 0;
 let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
 console.log(numeroSecreto);
 
 function asignarTextoElemento(elemento, texto){
@@ -33,18 +36,40 @@ function limpiarCaja(){
 }
 
 function generarNumeroSecreto(){
-     return Math.floor(Math.random()*10)+1;
+     let numeroGnerado = Math.floor(Math.random()*numeroMaximo)+1;
+
+     console.log(numeroGnerado);
+     console.log(listaNumerosSorteados);
+
+     //si el numero generado esta en la lista
+     if(listaNumerosSorteados.length == numeroMaximo){
+          asignarTextoElemento('p', 'Ya se sorteron todos los numeros posibles');
+          }else{
+               if(listaNumerosSorteados.includes(numeroGnerado)){
+                    return generarNumeroSecreto();
+
+               }else{
+                    listaNumerosSorteados.push(numeroGnerado);
+                    return numeroGnerado;
+               }
+          }
 }
 function condicionesIniciales(){
      asignarTextoElemento('h1', 'juego del numero secreto!');
-     asignarTextoElemento('p', 'Indica un numero del 1 al 100');
+     asignarTextoElemento('p', `Indica un numero del 1 al ${numeroMaximo}`);
      numeroSecreto = generarNumeroSecreto();
      intentos = 1;
 }
 
 function reiniciarJuego(){
+     //limpiar cja
      limpiarCaja();
+     //indicar mensaje de intervalo de numeros
+     //Generar eñ numero aleatorio
+     //Inicializar el numero de intentos
      condicionesIniciales();
+     //Deshabilitar el boton de nuevo juego
+     document.querySelector('#reiniciar').setAttribute('disabled', 'true');
      
 }
 condicionesIniciales();
--- ejerciciosAluraJs/js-curso-2-main/app.js
+++ ejerciciosAluraJs/js-curso-2-main/app.js
@@ -1,18 +1,12 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Hora del Desafio";
-
-function botonclick (){
-    console.log("el boton fue clicado");
+let array = [2,4,,3,5,78,9];
+let i = 0;
+function alCuadrado(){
+  if(i < array.length){ 
+  array[i] = array[i]*2;
+  i++;
+  alCuadrado()
+  }
+  
+  console.log(array);
 }
- function ciudadBrasil (){
-    let ciudad = prompt("¿dime una ciudad de Brasil?");
-    alert(`estuve en ${ciudad}`);
- }
- function mensajeGay(){
-    alert("Yo amo js");
- }
- function sumas (){
-    let num1 = parseInt(prompt("ingresa un numero a sumar"));
-    let num2 = parseInt(prompt("Ingresa un segundo numero a sumar"));
-    alert(`${num1} + ${num2} = ${num1+num2}`);
- }
+alCuadrado();
