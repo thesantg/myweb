1b537fde6d631716cf35a3531d91aa7d5bd92199
thesantg <santiago_calderon_vargas@hotmail.com>
2025-05-09 06:21:46 -0600

    actualizacion 09/05/2025

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo = [];
+
+function agregarAmigo(){
+    input = document.getElementById("amigo");
+    nombre = input.value;
+    if (nombre === ""){
+        alert("agregaste texto vacio, por favor agregar un amigo")
+    }else{
+        amigo.push(nombre);
+        console.log(amigo)
+        input.value = "";
+    }
+
+}
+agregarAmigo();
