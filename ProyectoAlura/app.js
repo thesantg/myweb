c1462b2540da0bec7bc82c27802a06cdba62cad5
thesantg <santiago_calderon_vargas>
2025-05-29 06:05:07 -0600

    actializacion 29/05/2025

--- ProyectoAlura/app.js
+++ ProyectoAlura/app.js
@@ -1,16 +1,16 @@
 //variables
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = 'vez';
+let maximosIntentos = 3;
 
     while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor:"));
 
     console.log(numeroUsuario);
     if (numeroUsuario == numeroSecreto) {
         //Acertamos, fue verdadera la condición
-        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? "vez" : "veces"}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
@@ -18,10 +18,10 @@ let palabraVeces = 'vez';
             alert('El número secreto es mayor');
         }
         //Incrementamos el contador cuando no acierta
-        intentos = intentos + 1;
-        palabraVeces = 'veces';
-        if(intentos > 3){
-            alert('Llegaste al numero maximo de intentos')
+        intentos++;
+      
+        if(intentos > maximosIntentos){
+            alert(`Llegaste al numero maximo de ${maximosIntentos} intentos`)
                 break;
         }
         //La condición no se cumplió
