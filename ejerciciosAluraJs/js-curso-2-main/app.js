713e081e9d1d2f177cf4b6640fed46c05c2e4226
thesantg <santiago_calderon_vargas>
2025-05-30 06:47:56 -0600

    actualizacion

--- ejerciciosAluraJs/js-curso-2-main/app.js
+++ ejerciciosAluraJs/js-curso-2-main/app.js
@@ -1,12 +1,3 @@
-//Aprobacion por nota
-//Declaracion de variable
-let notaUsuario = parseInt(prompt('Ingrese su Nota?'));
-  if(notaUsuario  >= 90 && notaUsuario <= 100){
-    console.log('Excelente');
-    }else if(notaUsuario >= 70 && notaUsuario <= 89){
-      console.log('Aprobado');
-      }else if(notaUsuario >= 0 && notaUsuario <= 69){
-        console.log('Reprobado');
-        }else{
-          console.log('Nota no valida');
-        }
+//Numero alatorio
+let numero = Math.round(Math.random()*10)+1;
+console.log(numero);
