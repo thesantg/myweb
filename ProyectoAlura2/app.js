d839b357fe411e19c33b553d5d5b60817c3ea98d
thesantg <santiago_calderon_vargas>
2025-06-02 06:01:39 -0600

    actualizacion 02/06/2025

--- ProyectoAlura2/app.js
+++ ProyectoAlura2/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = "Juego del numero secreto";
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = "Indica un numero del 1 al 10";
+
+function intentoDeUsuario (){
+    alert("click desde el boton");
+}
