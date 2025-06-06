ad256625448d737db589e20e4306d8cf6276601f
thesantg <santiago_calderon_vargas>
2025-06-06 06:27:43 -0600

    actualizacion

--- ProyectoAlura2/app.js
+++ ProyectoAlura2/app.js
@@ -1,11 +1,29 @@
+let numeroSecreto = generarNumeroSecreto();
+
+
 
 function asingnarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
 
-function intentoDeUsuario (){
-    alert("click desde el boton");
+function verificarIntento (){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(numeroDeUsuario);
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroDeUsuario === numeroSecreto);
+    
+
+    return;
+}
+
+
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
 }
 
 asingnarTextoElemento('h1', 'juego del numero secreto');
--- ejerciciosAluraJs/js-curso-2-main/js-curso-2-main/app.js
+++ ejerciciosAluraJs/js-curso-2-main/js-curso-2-main/app.js
@@ -1,4 +1,4 @@
-let tituloh1 = document.querySelector("h1");
+/*let tituloh1 = document.querySelector("h1");
 tituloh1.innerText = "Hora del desafio";
 function clickeado() {
     console.log("el boton fue clickeado");
@@ -17,4 +17,19 @@ function Lasuma() {
     let valor1 = parseInt(prompt("ingresa un numero"));
     let valor2 = parseInt(prompt("ingresa un segundo numero"));
     alert(`la suma de ${valor1} + ${valor2} = ${(valor1+valor2)}`)
+} */
+
+
+/* ejericio una funcion de suma con retorno de resultados
+
+function sumando (a,b){
+    return (a+b);
 }
+console.log(sumando(2, 5)); */
+
+
+/*ejercio de una funcion que devuelva un mensaje con nombre de un prompt
+function Mensaje(nombre) {
+    return `Hola ${nombre} Bienvenido`;
+}
+console.log(Mensaje("santiago"));*/
