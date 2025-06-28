71bc067f9824d88d9c012e7f57e1e98e1137ae0f
thesantg <santiago_calderon_vargas>
2025-06-28 06:19:45 -0600

    segunda actualizacion de 28/06/2025

--- ejerciciosAluraJs/js-curso-2-main/js-curso-2-main/app.js
+++ ejerciciosAluraJs/js-curso-2-main/js-curso-2-main/app.js
@@ -221,3 +221,5 @@ function encontrarNumero(){
 }
 encontrarNumero();
 */
+
+//Crea una función que reciba dos listas de números del mismo tamaño y devuelva una nueva lista con la suma de los elementos uno a uno
