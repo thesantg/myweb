0c587a566f247ebb6424e75244f4dee384c17f56
thesantg <santiago_calderon_vargas>
2025-06-28 06:18:19 -0600

    actualizacion 28/06/2025

--- ejerciciosAluraJs/js-curso-2-main/js-curso-2-main/app.js
+++ ejerciciosAluraJs/js-curso-2-main/js-curso-2-main/app.js
@@ -206,3 +206,18 @@ function sumasLista (){
 }
 sumasLista();
 */
+
+/*
+//Crea una función que devuelva la posición en la lista donde se encuentra un elemento pasado como parámetro, o -1 si no existe en la lista.
+let listaNumers = [1,2,3,4,5,6,7,8,9,10];
+
+function encontrarNumero(){
+    let numeroDeUsuario = parseInt(prompt("ingresa un numero"));
+    for(let i = 0; i <= listaNumers.length; i++){
+        if(numeroDeUsuario == listaNumers[i]){
+            console.log(`la pocision es : ${i} y ${listaNumers[i]}`)
+        }
+    }
+}
+encontrarNumero();
+*/
